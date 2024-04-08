32583a4b50621c4194ff32ab34043fc5ae14b3d5
3d014 <drapic397@gmail.com>
2024-04-08 09:47:05 +0200

    wip

--- src/skeleton/skeleton.tsx
+++ src/skeleton/skeleton.tsx
@@ -1,40 +1,33 @@
-// import { Box } from "@mui/material"
-import Login from "../views/login/login"
-// import styles from "./skeleton.styles"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./components/layout/layout"
-import RequireAuth from "../utils/protectedRoutes/requireAuth"
-import Home from "../views/home/home"
-import Test from "../views/test"
-import { AnonymousRoute } from "../utils/anonymousRoutes/anonymousRoues"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import RequireAuth from "../utils/protectedRoutes/requireAuth";
+import Home from "../views/home/home";
+import Test from "../views/test";
+import Login from "../views/login/login";
+import { AnonymousRoute } from "../utils/anonymousRoutes/anonymousRoues";
+
+const Skeleton = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+       
+ <Route element={<AnonymousRoute />}>
+<Route path='/login'  element={ <Login />} />
+</Route> 
+        {/* Anonimna ruta za /test */}
+        <Route path="/test" element={<Test />} />
+        {/* Rute koje zahtijevaju autentikaciju i autorizaciju */}
+        <Route
+          element={<RequireAuth allowedRoles={['admin']} />}
+        >
+          {/* Sve rute koje zahtijevaju autentikaciju i autorizaciju bit će omotane s Layout komponentom */}
+          <Route path="/*" element={<Layout><Home /></Layout>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Skeleton;
 
 
-
-const Skeleton=()=>{
-
-
-
-return <Layout>
-
-<BrowserRouter>
-    
-    <Routes>
-    <Route element={<AnonymousRoute />}>
-    <Route path='/login'  element={ <Login />} />
-    </Route> 
-    
-    <Route path='/test' element={<Test></Test>}></Route>
-    <Route element={<RequireAuth allowedRoles={['admin']} />}>
-          <Route path="/*" element={<Home/>} />
-    </Route> 
-    </Routes>
-
-</BrowserRouter>
-
- 
-
-</Layout>
-
-}
-
-export default Skeleton
